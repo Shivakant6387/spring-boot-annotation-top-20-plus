99bf0db8c2504b2e9d56e12c39e6d9a40b6657bb
Shivakant6387 <shivakantsingh900@gmail.com>
2024-03-30 17:07:48 +0530

    response xml

--- src/main/java/com/example/Spring/boot/annotation/controller/TutorialController.java
+++ src/main/java/com/example/Spring/boot/annotation/controller/TutorialController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/tutorial")
+@RequestMapping(path = "/api/tutorial", produces = "application/xml")
 public class TutorialController {
     @Autowired
     TutorialRepository tutorialRepository;
